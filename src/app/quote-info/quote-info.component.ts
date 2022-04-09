8bd756efe2f29e5844da7b1542db9a10337178fe
gladyswambura <gladyswahito7@gmail.com>
2022-04-09 11:57:09 +0300

    Fix: fixes a big in the quote array

--- src/app/quote-info/quote-info.component.ts
+++ src/app/quote-info/quote-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-info',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-info.component.css']
 })
 export class QuoteInfoComponent implements OnInit {
+
+  quotes:Quote[]=[
+      new Quote(1, 'if you want the rainbow, you must put up with the rain', 0, 0),
+      new Quote(2, 'if you want the rainbow, you must put up with the rain', 0, 0),
+      new Quote(3, 'if you want the rainbow, you must put up with the rain', 0, 0)
+  ]
 
   constructor() { }
 
